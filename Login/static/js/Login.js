64a8efed0a35714d94786a747a0aa48b3ad26363
chickiyeah <55694044+chickiyeah@users.noreply.github.com>
2022-12-24 17:47:32 +0900

    354567464

--- Login/static/js/Login.js
+++ Login/static/js/Login.js
@@ -1,3 +1,37 @@
+/*
+ * Create form to request access token from Google's OAuth 2.0 server.
+ */
+function GoogleOauthSignIn() {
+  // Google's OAuth 2.0 endpoint for requesting an access token
+  var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+  // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+  var form = document.createElement('form');
+  form.setAttribute('method', 'GET'); // Send as a GET request.
+  form.setAttribute('action', oauth2Endpoint);
+
+  // Parameters to pass to OAuth 2.0 endpoint.
+  var params = {'client_id': '718070206861-ncufgj5140jkdj39latro6os54jnqptl.apps.googleusercontent.com',
+                'redirect_uri': 'http://daily-log.or.kr/Login',
+                'response_type': 'token',
+                'scope': 'email profile openid https://www.googleapis.com/auth/user.birthday.read',
+                'include_granted_scopes': 'true',
+                'state': 'pass-through value'};
+
+  // Add form parameters as hidden input values.
+  for (var p in params) {
+    var input = document.createElement('input');
+    input.setAttribute('type', 'hidden');
+    input.setAttribute('name', p);
+    input.setAttribute('value', params[p]);
+    form.appendChild(input);
+  }
+
+  // Add form to page and submit it to open the OAuth 2.0 endpoint.
+  document.body.appendChild(form);
+  form.submit();
+}
+
 function handleCredentialResponse(response) {
   // decodeJwtResponse() is a custom function defined by you
   // to decode the credential response.
